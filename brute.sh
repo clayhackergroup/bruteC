c178a9cdf1c3d099753e0d2104946ff38fa567d4
clayhackergroup <desksecuraty@gmail.com>
2024-09-25 20:57:53 +0530

    Create brute.sh

---
+++ brute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+figlet -f big -w 100 BRUTEC| lolcat
+figlet -f small -w 100 clay hacker group | lolcat                      
+echo -e "\033[1;31mThis tool helps to do brute force on instagrm login pages  for only ethical purpose \033[1;34mWe on social media instagram:h4cker.in Youtube:h4cker_in Telegram: claysecure23 \033[0m"
+python3 Brute.py
